3c80b2c4cd8e722d70a1033854e6e2114bc0d6e0
Dalton Craven <daltoncraven@protonmail.com>
2022-06-09 20:30:02 +0200

    test: write tests for getValidKey

--- src/__tests__/keys.test.ts
+++ src/__tests__/keys.test.ts
@@ -1,4 +1,4 @@
-import { isValidKey } from '../keys';
+import { isValidKey, getValidKey } from '../keys';
 
 describe('isValidKey()', () => {
   it('should return false if a key is undefined', () => {
@@ -11,5 +11,19 @@ describe('isValidKey()', () => {
 
   it('should return true if a key is in the object', () => {
     expect(isValidKey('key1', { key1: 'test' })).toBe(true);
+  });
+});
+
+describe('getValidKey()', () => {
+  it('should return undefined if the key is not in the object and no default value is provided', () => {
+    expect(getValidKey('abc', { en: 1, fr: 2 })).toBeUndefined();
+  });
+
+  it('should return the default value if the key is not in the object and a default value is provided', () => {
+    expect(getValidKey('abc', { en: 1, fr: 2 }, 'en')).toBe('en');
+  });
+
+  it('should return the key if the key is in the object', () => {
+    expect(getValidKey('abc', { abc: 1, en: 2 }, 'en')).toBe('abc');
   });
 });
