bfd6ccd19c230215a8808797ce2e4d7115004588
Dalton Craven <daltoncraven@protonmail.com>
2022-07-05 11:51:57 +0200

    refactor: simplify variable names

--- src/keys.ts
+++ src/keys.ts
@@ -15,35 +15,26 @@ export function isValidKey<T extends Record<string, unknown>>(
 ): keyToTest is keyof T {
   return keyToTest in objectToTest;
 }
-
-/**
- * This function takes in a key to test, an object to test, and an optional
- * default value. If the key is present in the object, it will return the
- * key. If the key is not present in the object, it will return the default
- * value. If the default value is not provided, it will return undefined.
- *
- * @param keyToTest The key that should be tested (must be a valid key type).
- * @param objectToTest The object that should be tested (must be an object).
- * @param defaultValue An optional default value that will be returned
- * if the key is not present in the object.
- *
- * @returns The key if it is in the object, the default value if the key
- * is not present in the object, or undefined.
- */
 export function getValidKey<T extends Record<string, unknown>>(
-  keyToTest: string | number | symbol,
-  objectToTest: T,
+  key: string | number | symbol,
+  object: T,
   defaultValue: keyof T
 ): keyof T;
 
+export function getValidKey<T extends Record<string, unknown>>(
+  key: string | number | symbol,
+  object: T,
+  defaultValue?: undefined
+): keyof T | undefined;
+
 /**
- * This function takes in a key to test, an object to test, and an optional
+ * This function takes in a key, an object, and an optional
  * default value. If the key is present in the object, it will return the
  * key. If the key is not present in the object, it will return the default
  * value. If the default value is not provided, it will return undefined.
  *
- * @param keyToTest The key that should be tested (must be a valid key type).
- * @param objectToTest The object that should be tested (must be an object).
+ * @param key The key that should be tried (must be a valid key type).
+ * @param object The object to be accessed (must be an object).
  * @param defaultValue An optional default value that will be returned
  * if the key is not present in the object.
  *
@@ -51,15 +42,9 @@ export function getValidKey<T extends Record<string, unknown>>(
  * is not present in the object, or undefined.
  */
 export function getValidKey<T extends Record<string, unknown>>(
-  keyToTest: string | number | symbol,
-  objectToTest: T,
-  defaultValue?: undefined
-): keyof T | undefined;
-
-export function getValidKey<T extends Record<string, unknown>>(
-  keyToTest: string | number | symbol,
-  objectToTest: T,
+  key: string | number | symbol,
+  object: T,
   defaultValue?: keyof T,
 ) {
-  return keyToTest in objectToTest ? (keyToTest as keyof T) : defaultValue;
+  return key in object ? (key as keyof T) : defaultValue;
 }
