8fed4b0b1192dc17f1fdd3551ff098c493450941
Dalton Craven <daltoncraven@protonmail.com>
2022-07-06 16:22:12 +0200

    test: simplify tests

--- src/__tests__/keys.test.ts
+++ src/__tests__/keys.test.ts
@@ -2,21 +2,22 @@ import { isValidKey, getValidKey } from "../keys";
 
 describe("isValidKey()", () => {
   it("should return false if the key is undefined", () => {
-    expect(isValidKey({}, undefined)).toBe(false);
+    expect(isValidKey({ en: 1, fr: 2 }, undefined)).toBe(false);
   });
   it("should return false if the key is not a valid key type", () => {
-    expect(isValidKey({ test: true }, { test: true })).toBe(false);
+    expect(isValidKey({ en: 1, fr: 2 }, true)).toBe(false);
+    expect(isValidKey({ en: 1, fr: 2 }, { test: true })).toBe(false);
   });
 
   it("should return false if the key is not in the object", () => {
-    expect(isValidKey({ key1: "test" }, "abc")).toBe(false);
+    expect(isValidKey({ en: 1, fr: 2 }, "abc")).toBe(false);
   });
 
   it("should return true if the key is in the object", () => {
     const testSymbol = Symbol("test symbol");
-    expect(isValidKey({ key1: "test" }, "key1")).toBe(true);
-    expect(isValidKey({ 1: "test" }, 1)).toBe(true);
-    expect(isValidKey({ [testSymbol]: "test" }, testSymbol)).toBe(true);
+    expect(isValidKey({ en: 1, fr: 2 }, "en")).toBe(true);
+    expect(isValidKey({ 1: "en", 2: "fr" }, 1)).toBe(true);
+    expect(isValidKey({ [testSymbol]: "en" }, testSymbol)).toBe(true);
   });
 });
 
--- src/__tests__/objects.test.ts
+++ src/__tests__/objects.test.ts
@@ -6,6 +6,7 @@ describe("getItem()", () => {
   });
 
   it("should return undefined if the key is not a valid key type", () => {
+    expect(getItem({ en: 1, fr: 2 }, true)).toBeUndefined();
     expect(getItem({ en: 1, fr: 2 }, { test: true })).toBeUndefined();
   });
 
@@ -13,21 +14,22 @@ describe("getItem()", () => {
     expect(getItem({ en: 1, fr: 2 }, "abc")).toBeUndefined();
   });
 
+  it("should return undefined if the key is undefined and a default value is undefined", () => {
+    expect(getItem({ en: 1, fr: 2 }, undefined, undefined)).toBeUndefined();
+  });
+
   it("should return the default value if the key is not in the object and a default value is provided", () => {
-    expect(getItem({ en: 1, fr: 2 }, "abc", "en")).toBe("en");
+    expect(getItem({ en: 1, fr: 2 }, "abc", 3)).toBe(3);
   });
 
   it("should return the default value if the key is undefined and a default value is provided", () => {
-    expect(getItem({ en: 1, fr: 2 }, undefined, "en")).toBe("en");
-  });
-  it("should return the key if the key is in the object", () => {
-    expect(getItem({ en: 1, fr: 2 }, "fr", 1)).toBe(2);
+    expect(getItem({ en: 1, fr: 2 }, undefined, 3)).toBe(3);
   });
 
   it("should return the item if the key is in the object", () => {
     const testSymbol = Symbol("test symbol");
-    expect(getItem({ key1: "test" }, "key1")).toBe("test");
-    expect(getItem({ 1: "test" }, 1)).toBe("test");
-    expect(getItem({ [testSymbol]: "test" }, testSymbol)).toBe("test");
+    expect(getItem({ en: 1, fr: 2 }, "en")).toBe(1);
+    expect(getItem({ 1: "en", 2: "fr" }, 1)).toBe("en");
+    expect(getItem({ [testSymbol]: "en" }, testSymbol)).toBe("en");
   });
 });
